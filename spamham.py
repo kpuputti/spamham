0406bc088e1b4f2ab9ea3456db6f9196e483bf27
Kimmo Puputti <kpuputti@gmail.com>
2010-11-18 20:26:50 +0200

    Documentation and usage help.

--- spamham.py
+++ spamham.py
@@ -1,7 +1,25 @@
+"""\
+spamham.py - spam/ham classifier
+
+Usage:
+python spamham.py [classifier] [args]
+
+where [classifier] is the name of the classifier and [args] is a list
+of arguments for the classifier."""
 import sys
 
 
+def usage():
+    """Print the usage help from module doc."""
+    print __doc__
+
+
 def main(args):
+    """Dispatch the functionality based on the argument list."""
+    if len(args) == 0:
+        sys.stderr.write('Error: No method specified\n')
+        usage()
+        sys.exit(2)
     return 0
 
 
