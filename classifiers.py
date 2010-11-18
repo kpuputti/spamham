f83621a3f106d7b7e5f8674fb85c205a7e0a47a0
Kimmo Puputti <kpuputti@gmail.com>
2010-11-18 22:27:49 +0200

    Added classifier names and a method to split the data.

--- classifiers.py
+++ classifiers.py
@@ -4,18 +4,33 @@ import random
 class Classifier(object):
     """Base class for all classifiers."""
 
-    def __init__(self, trainset):
-        self.trainset = trainset
+    name = 'base classifier'
+
+    def __init__(self, data):
+        self.data = data
+        self.trainset, self.testset = self.split_data(data)
+
+    def split_data(self, data):
+        """Split the data set randomly into two sets of ids from the
+        first column of the data set."""
+        ids = set(d[0] for d in data)
+        trainset = set(random.sample(ids, len(ids) / 2))
+        testset = ids - trainset
+        return trainset, testset
 
     def train(self):
         raise NotImplementedError('Implement this method in a derived class.')
 
     def classify(self, datum):
+        """Return a Boolean value indicating whether the given datum
+        is spam or ham."""
         raise NotImplementedError('Implement this method in a derived class.')
 
 
 class RandomClassifier(Classifier):
     """Classify data randomly."""
+
+    name = 'random classifier'
 
     def train(self):
         pass
@@ -26,6 +41,8 @@ class RandomClassifier(Classifier):
 
 class SVMClassifier(Classifier):
     """Classify the data using Support Vector Machines."""
+
+    name = 'SVM classifier'
 
     def train(self):
         pass
