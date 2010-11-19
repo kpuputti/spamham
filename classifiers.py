f4b7a9dcb7c3bcb010217bc9e78b691a5495e8cc
Kimmo Puputti <kpuputti@gmail.com>
2010-11-19 19:52:45 +0200

    Implemented the DummyClassifier.

--- classifiers.py
+++ classifiers.py
@@ -1,3 +1,4 @@
+import collections
 import random
 
 
@@ -74,11 +75,45 @@ class DummyClassifier(Classifier):
 
     name = 'dummy classifier'
 
+    # Frequency in all spam messages needed for a field to be a spam
+    # field.
+    SPAM_FIELD_FREQ = 0.5
+    # Frequency of spam fields needed within all spam fields for a
+    # datum to be a spam datum.
+    SPAM_FIELDS_FREQ = 0.5
+
+    def __init__(self, data):
+        self.spam_colums = None
+        super(DummyClassifier, self).__init__(data)
+
     def train(self):
-        pass
+        num_spams = 0
+        spam_columns = collections.defaultdict(int)
+        for datum in self.trainset:
+            # Skip ham datums.
+            if datum[1] == 0:
+                continue
+            num_spams += 1
+            for i in xrange(2, len(datum)):
+                if datum[i]:
+                    spam_columns[i] += 1
+        spam_freq_needed = self.SPAM_FIELD_FREQ * num_spams
+        spams = set()
+        # Add all columns with a big enough count to spam column.
+        for column, count in spam_columns.iteritems():
+            if count >= spam_freq_needed:
+                spams.add(column)
+        self.spam_columns = spams
+
 
     def classify(self, datum):
-        pass
+        assert self.spam_columns is not None, 'Train the classifier first!'
+        spam_columns_needed = self.SPAM_FIELDS_FREQ * len(self.spam_columns)
+        num_spam_columns = 0
+        for column in self.spam_columns:
+            if datum[column]:
+                num_spam_columns += 1
+        return num_spam_columns >= spam_columns_needed
 
 
 class SVMClassifier(Classifier):
